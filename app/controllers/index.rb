d8d51b9f3dd03890aa1d718ec9a701595898f4b6
Kaelin Burns <kaelinbu@gmail.com>
2014-05-07 20:02:04 -0700

    button options stored in a hash

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,12 +1,19 @@
 require 'httparty'
 
 get '/' do
+  @choices = {happy: "HAPPY",
+              friends: "FRIENDLY",
+              peaceful: "PEACEFUL",
+              grateful: "GRATEFUL",
+              selfie: "SELF CENTERED",
+              love: "LOVEY"}
   erb :index
 end
 
 get '/images' do
   @keyword = params[:tag]
-  @images = Image.where(tag: @keyword).map {|image| image.url}
+  @images = Image.where(tag: @keyword).shuffle.first(20)
+  @images.map! {|image| image.url}
   if @images.empty?
     client = Instagram::Client.new
     @images = client.get_images(@keyword)
