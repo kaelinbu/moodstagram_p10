d73bd6bb946defcd711af6b40f7a9b3e4e1a4530
Kaelin Burns <kaelinbu@gmail.com>
2014-05-07 18:48:33 -0700

    returns random sample of 20 images every time you click a mood button

--- app/models/instagram.rb
+++ app/models/instagram.rb
@@ -5,15 +5,15 @@ module Instagram
 
     def get_images(keyword)
       response = self.class.get("/v1/tags/#{keyword}/media/recent?client_id="+ENV['INSTAGRAM_KEY'],
-        :query => {count:30,
-                    max_id: 1399377631050085}
-                    # max_id = ["pagination"]["next_max_tag_id"]- can interate through a given number of times to get multiple pages
+        :query => {}
         )
-      parseForImages(response, keyword)
+      checkAndCreateImages(response,keyword)
     end
 
-    def parseForImages(responseHash, keyword)
-      return responseHash["data"].map {|post| Image.create(tag: keyword, url: post["images"]["standard_resolution"]["url"])}
+    def checkAndCreateImages(responseHash, keyword)
+      if Image.find_by_url(responseHash[:url]) == nil
+       responseHash["data"].map {|post| Image.create(tag: keyword, url: post["images"]["standard_resolution"]["url"])}
+     end
     end
   end
 end
