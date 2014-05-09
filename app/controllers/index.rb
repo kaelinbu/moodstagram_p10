3be97be58e6a4256607ffb222ad8965483a745b6
Kaelin Burns <kaelinbu@gmail.com>
2014-05-08 17:40:06 -0700

    oauth with instagram gem up and running

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,13 @@
-require 'httparty'
+# require 'httparty'
+enable :sessions
+
+CALLBACK_URL = "http://localhost:9393/oauth/callback"
+
+Instagram.configure do |config|
+  config.client_id = '2c3d8f2235aa415e942b7859632002b4'
+  config.client_secret = 'fd9ca2da3eb4411f92ae6494ac37c1da'
+end
+
 
 get '/' do
   @choices = ChoiceRetriever.return_choices
@@ -21,4 +30,14 @@ end
 get '/update' do
   @choices = ChoiceRetriever.return_choices
   erb :_buttons, layout: false
+end
+
+get "/oauth/connect" do
+  redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+end
+
+get "/oauth/callback" do
+  response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+  session[:access_token] = response.access_token
+  redirect "/"
 end
